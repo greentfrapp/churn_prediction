e4f4d386ca989506ba4dfa7c1993ad16caa6597d
Lim Swee Kiat <limsweekiat@gmail.com>
2020-07-13 20:48:51 +0800

    Add tensorflow model

--- serve.py
+++ serve.py
@@ -5,9 +5,7 @@ import json
 import pickle
 
 import numpy as np
-import torch
-from torch import nn
-import torch.nn.functional as F
+import tensorflow as tf
 
 from utils.constants import AREA_CODES, STATES, SUBSCRIBER_FEATURES
 
@@ -42,27 +40,9 @@ def pre_process(http_body):
     return np.array(row_feats).reshape(1, -1)
 
 
-class TorchModel(nn.Module):
-    def __init__(self, input_dim):
-        super(TorchModel, self).__init__()
-        self.fc1 = nn.Linear(input_dim, 1000)
-        self.fc2 = nn.Linear(1000, 1000)
-        self.fc3 = nn.Linear(1000, 1)
-
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
-
 class Model:
     def __init__(self):
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        self.model = TorchModel(input_dim=66)
-        self.model.load_state_dict(torch.load("/artefact/lgb_model.pkl"))
-        self.model.to(self.device)
-        self.model.eval()
+        self.model = tf.keras.models.load_model('/artefact/my_model')
 
     def predict(self, features):
-        return F.sigmoid(self.model(torch.tensor(np.array(features)).float().to(self.device))).cpu().detach().item()
+        return self.model(features).numpy()
--- train.py
+++ train.py
@@ -11,9 +11,7 @@ from bedrock_client.bedrock.api import BedrockApi
 from bedrock_client.bedrock.metrics.service import ModelMonitoringService
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
-import torch
-from torch import nn, optim
-import torch.nn.functional as F
+import tensorflow as tf
 
 from utils.constants import FEATURE_COLS, TARGET_COL
 
@@ -25,24 +23,10 @@ N_ESTIMATORS = int(os.getenv("N_ESTIMATORS"))
 OUTPUT_MODEL_NAME = os.getenv("OUTPUT_MODEL_NAME")
 
 
-class TorchModel(nn.Module):
-    def __init__(self, input_dim):
-        super(TorchModel, self).__init__()
-        self.fc1 = nn.Linear(input_dim, 1000)
-        self.fc2 = nn.Linear(1000, 1000)
-        self.fc3 = nn.Linear(1000, 1)
-
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
-
 def compute_log_metrics(clf, x_val, y_val, device):
     """Compute and log metrics."""
     print("\tEvaluating using validation data")
-    y_prob = F.sigmoid(clf(torch.tensor(np.array(x_val)).float().to(device))).cpu().detach().numpy()
+    y_prob = clf(x_val).numpy()
     y_pred = (y_prob > 0.5).astype(int)
 
     acc = metrics.accuracy_score(y_val, y_pred)
@@ -84,26 +68,28 @@ def main():
     )
 
     print("\tTrain model")
-    clf = TorchModel(
-        input_dim=x_train.shape[1]
-    ).to(device)
-    optimizer = optim.Adam(clf.parameters(), lr=0.001)
-    loss_fn = nn.BCEWithLogitsLoss()
-    batchsize = 16
-    for t in range(len(x_train) // batchsize):
-        optimizer.zero_grad()
-        batch_x = x_train[t * batchsize:(t + 1) * batchsize]
-        batch_y = y_train[t * batchsize:(t + 1) * batchsize]
-        logits = clf(torch.tensor(np.array(batch_x)).float().to(device))
-        loss = loss_fn(logits, torch.tensor(np.array(batch_y)).float().to(device).view(-1, 1))
-        loss.backward()
-        optimizer.step()
-    compute_log_metrics(clf, x_val, y_val, device)
+    clf = tf.keras.models.Sequential([
+      tf.keras.layers.Dense(1000, activation="relu"),
+      tf.keras.layers.Dense(1000, activation="relu"),
+      tf.keras.layers.Dense(1)
+    ])
+
+    loss_fn = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+    clf.compile(
+        optimizer="adam",
+        loss=loss_fn,
+        metrics=["accuracy"]
+    )
+    clf.fit(x_train, y_train, epochs=5)
+
+    clf_prob = tf.keras.Sequential([clf, tf.keras.layers.Activation('sigmoid')])
+    
+    compute_log_metrics(clf_prob, x_val, y_val, device)
 
     print("\tComputing metrics")
     selected = np.random.choice(model_data.shape[0], size=1000, replace=False)
     features = model_data[FEATURE_COLS].iloc[selected]
-    inference = F.sigmoid(clf(torch.tensor(np.array(features)).float().to(device))).cpu().detach().numpy()
+    inference = clf_prob(features).numpy()
 
     ModelMonitoringService.export_text(
         features=features.iteritems(),
@@ -111,7 +97,7 @@ def main():
     )
 
     print("\tSaving model")
-    torch.save(clf.state_dict(), "/artefact/" + OUTPUT_MODEL_NAME)
+    clf_prob.save("/artefact/" + OUTPUT_MODEL_NAME)
 
 
 if __name__ == "__main__":
