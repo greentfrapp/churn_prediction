badedc60f1f0f2e09ff86f71587e219c363e3a97
Lim Swee Kiat <limsweekiat@gmail.com>
2020-07-23 11:26:53 +0800

    Read file instead of JSON

--- serve.py
+++ serve.py
@@ -1,39 +1,32 @@
 """
 Script for serving.
 """
-import json
-import pickle
+from PIL import Image
 
-import numpy as np
 import torch
-from torch import nn
-import torch.nn.functional as F
-from torchvision import transforms, models
+from torchvision.models import resnet50
+from torchvision.transforms import CenterCrop, Compose, Normalize, Resize, ToTensor
 
 
-def pre_process(http_body):
-    """Predict churn probability given subscriber_features.
-
-    Args:
-        subscriber_features (dict)
-        model
-
-    Returns:
-        churn_prob (float): churn probability
-    """
-    features = np.array(json.loads(http_body)["image"]) / 255
-    features_t = torch.tensor(features)
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                     std=[0.229, 0.224, 0.225])
-    return normalize(features_t).unsqueeze(dim=0).float()
+def pre_process(http_body, files):
+    return [Image.open(files["image"])]
 
 
 class Model:
     def __init__(self):
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        self.model = models.resnet50(pretrained=True)
+        self.model = resnet50(pretrained=True)
         self.model.to(self.device)
         self.model.eval()
+        self.transform = Compose(
+            [
+                Resize(256),
+                CenterCrop(224),
+                ToTensor(),
+                Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ]
+        )
 
     def predict(self, features):
-        return torch.argmax(self.model(features).squeeze()).cpu().detach().item()
+        features_t = self.transform(features[0]).unsqueeze_(0)
+        return self.model(features_t).max(1)[1].item()
